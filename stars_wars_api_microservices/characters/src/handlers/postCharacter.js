8e05da97f4057e404a35618c36d279ca35548fb7
Oscar Restrepo <oscar.restrepo2@gmail.com>
2023-09-19 16:52:05 -0500

    feat(character_server): add util response

--- stars_wars_api_microservices/characters/src/handlers/postCharacter.js
+++ stars_wars_api_microservices/characters/src/handlers/postCharacter.js
@@ -1,10 +1,11 @@
 const controllers = require('../controllers/index');
+const { response } = require('../utils');
 
 const postCharacter = async (req, res) => {
   const result = await controllers.createCharacter;
   console.log(result);
 
-  res.status(201).send(result);
+  response(res, 200, result);
 };
 
 module.exports = postCharacter;
