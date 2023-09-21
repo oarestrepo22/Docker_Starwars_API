5b384a39094f7ee234df39dd7b08aac2076d5b38
Oscar Restrepo <oscar.restrepo2@gmail.com>
2023-09-21 13:53:40 -0500

    refac: index

--- stars_wars_api_microservices/gateway/index.js
+++ stars_wars_api_microservices/gateway/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(morgan('dev'));
 //app.use(express.json())
 
-app.use('/characters', createProxy('8001'));
+app.use('/characters', createProxy(8001));
 
 app.use('/films', createProxy(8002));
 
