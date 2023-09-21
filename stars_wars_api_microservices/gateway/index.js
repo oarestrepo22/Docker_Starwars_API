c066ad48fd5d76134aa0ed1273f6ab7c430ab821
Oscar Restrepo <oscar.restrepo2@gmail.com>
2023-09-21 12:25:21 -0500

    refac: proxy connection

--- stars_wars_api_microservices/gateway/index.js
+++ stars_wars_api_microservices/gateway/index.js
@@ -1,7 +1,11 @@
-const app = require('./src/app');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const express = require('express');
+const morgan = require('morgan');
 const { createProxy } = require('./src/utils');
 const PORT = 8000;
+
+const app = express();
+app.use(morgan('dev'));
+//app.use(express.json())
 
 app.use('/characters', createProxy('8001'));
 
@@ -9,4 +13,4 @@ app.use('/films', createProxy(8002));
 
 app.use('/planets', createProxy(8003));
 
-app.listen(PORT, console.log(`Server running on port ${PORT}`));
+app.listen(PORT, console.log(`Gateway on port ${PORT}`));
